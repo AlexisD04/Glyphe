e01a90320d8ed9a1decd59938186dbd4320fb7ab
Alexis <alexis@localhost.localdomain>
2025-11-27 18:49:56 +0100

    Modified user's data

--- core/config.py
+++ core/config.py
@@ -1,5 +1,4 @@
 users = {
-    "alexis":"0d205be961aea85e5cb046e5b7ae2d6d76629ac3a66e27556944c7476f40f550",
     "guest":"84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec"}
 
 COLORS = {
@@ -22,6 +21,6 @@ COLORS_CURSOR = {
     "white":   "\033]12;#FFFFFF\007"
 }
 
-SHODAN_API_KEY = "03MEnt7uCVDXs08HPwcFdxPklnNcdC0c"
+SHODAN_API_KEY = ""
 
 main_color = "white"
